a9da408d08edf106d3b0cb0741bf27d6672f76b5
Frans Paasonen <franspaasonen@gmail.com>
2020-05-17 19:29:06 +0300

    Added telephone-line modeline

--- config/custom-packages.el
+++ config/custom-packages.el
@@ -97,6 +97,17 @@
   (setq awesome-tab-height 100)
   (awesome-tab-mode t))
 
+(use-package telephone-line
+  :straight t
+  :config
+  (setq telephone-line-primary-left-separator 'telephone-line-cubed-left
+        telephone-line-secondary-left-separator 'telephone-line-cubed-hollow-left
+        telephone-line-primary-right-separator 'telephone-line-cubed-right
+        telephone-line-secondary-right-separator 'telephone-line-cubed-hollow-right)
+  (setq telephone-line-height 24
+        telephone-line-evil-use-short-tag t)
+  (telephone-line-mode t))
+
 (use-package dired-sidebar
   :straight t
   :commands (dired-sidebar-toggle-sidebar)
